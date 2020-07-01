fc23527d0ce210dc3bed49e24ce304918350d304
ensarhamzic01 <ensarhamzic01@gmail.com>
2020-07-01 16:32:17 +0200

    Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,34 @@
 
 @section('content')
 <div class="container">
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Add New Task List</h4>
+                </div>
+                <div class="modal-body">
+                    <label for="taskListName">Task List Name</label>
+                    <input type="text" class="form-control" name="taskListName" id="taskListName">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Add</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <div class="row">
         <div class="col-md-3">
             <div class="card">
-                <div class="card-header font-weight-bold">Your notes</div>
+                <div class="card-header font-weight-bold">
+                    <span>Task Lists</span>
+                    <i class="fa fa-plus-square-o fa-2x float-right btn py-0 px-2" data-toggle="modal"
+                        data-target="#myModal"></i>
+                </div>
 
                 <div class="card-body">
                     List
@@ -14,7 +38,9 @@
         </div>
         <div class="col-md-9">
             <div class="card">
-                <div class="card-header">Your notes</div>
+                <div class="card-header">Your notes
+
+                </div>
 
                 <div class="card-body">
                     List
